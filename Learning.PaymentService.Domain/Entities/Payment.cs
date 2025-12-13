78965cb4d1caf0d14ff2cdb6425a395fb679b528
krunal-parmar24 <krunalrparmar4@gmail.com>
2025-12-13 19:02:24 +0530

    Added required empty project structure with domain entities

---
+++ Learning.PaymentService.Domain/Entities/Payment.cs
@@ -0,0 +1,16 @@
+﻿using Learning.PaymentService.Domain.Enums;
+
+namespace Learning.PaymentService.Domain.Entities
+{
+    /// <summary>
+    /// Represents a payment entity with details about the payment transaction.
+    /// </summary>
+    public class Payment : BaseEntity
+    {
+        public int OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public PaymentStatus Status { get; set; }
+        public string TransactionId { get; set; } = string.Empty;
+    }
+}
---
+++ Learning.PaymentService.Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Learning.PaymentService.Domain.Enums
+{
+    /// <summary>
+    /// Presents the various payment statuses in the payment service.
+    /// </summary>
+    public enum PaymentStatus
+    {
+        Pending = 0,
+        Success = 1,
+        Failed = 2,
+        Refunded = 3,
+    }
+}
