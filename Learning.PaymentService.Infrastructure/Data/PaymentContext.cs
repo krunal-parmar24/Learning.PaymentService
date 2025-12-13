561d592709f2ec81bb9b67b5d3b0a557671b366c
krunal-parmar24 <krunalrparmar4@gmail.com>
2025-12-13 19:25:14 +0530

    Added required files to generate initial migration

---
+++ Learning.PaymentService.Infrastructure/Data/PaymentContext.cs
@@ -0,0 +1,23 @@
+﻿using Learning.PaymentService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learning.PaymentService.Infrastructure.Data
+{
+    /// <summary>
+    /// Context class for PaymentService, managing Payment entities.
+    /// </summary>
+    public class PaymentContext : DbContext
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("payments");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
